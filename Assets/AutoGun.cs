3a2c274b4b98bb5181129767e7390c308fb44a57
Nazaire-S <nqs.realme@gmail.com>
2015-12-23 22:13:25 -0500

    Many Fixes (View Description)
    1. Player moves in the right direction, regardless or rotation.
    2. Player rotates to face the mouse, and thus shoots in the mouse's
    direction
    3. AutoGun rotates independently from player
    4. Bullets now travel in the right direction
    5. Bullets are now pooled for each gun
    6. Bullet's 'Destroy()' method is no longer destructive
    
    Note: Feels like the AutoGun and ControlGun classes can be made into one
    class with an 'isAuto' boolean.

--- Assets/AutoGun.cs
+++ Assets/AutoGun.cs
@@ -3,21 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AutoGun : MonoBehaviour {
-
 	public GameObject bullet;
+
+    int bulletIndex = 0;
+    int maxBullets = 50;
 	Vector3 spinVect = new Vector3(0, 0, 27.5f);
+    Quaternion rotation;
+
+    GameObject[] bullets;
 
 	void Awake() {
-		InvokeRepeating("Shoot", 1f, 1f);
-	}
+        rotation = transform.rotation;
+
+        bullets = new GameObject[maxBullets];
+        Vector3 offset = new Vector3(1000, 1000, 0);
+
+        for (int i = 0; i < maxBullets; i++){
+            bullets[i] = Instantiate(bullet, offset, transform.rotation) as GameObject;
+        }
+
+        InvokeRepeating("Shoot", 1f, 1f);
+    }
 
 	/* Rotates the gun around the player. */
-	void Update () {
-		transform.Rotate(spinVect * Time.deltaTime);
+	void LateUpdate () {
+        transform.rotation = rotation;
+        transform.Rotate(spinVect * Time.deltaTime);
+        rotation = transform.rotation;
 	}
 
 	/* Shoots according to the difficulty. */
 	void Shoot() {
-		Instantiate(bullet, new Vector2(transform.position.x, transform.position.y), transform.rotation);
+        bullets[bulletIndex].transform.GetComponent<Bullet>().Renew(transform.position, (transform.rotation * Vector3.up).normalized * 0.1f);
+        bulletIndex = (bulletIndex + 1) % maxBullets;
 	}
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,30 +2,51 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+    public KeyCode up, down, left, right;
+    public AudioClip lose;
+    new public Camera camera;
 
-	public KeyCode up, down, left, right;
 	float speed = .375f;
-	public AudioClip lose; 
-	AudioSource audio;
+    Vector3 screenPos;
 
-	void Awake () {
+    new AudioSource audio;
+
+    void Awake () {
 		audio = GetComponent<AudioSource>();
-	}
+    }
 
 	void Update () {
 		Movement();
+        Rotation();
 	}
 
+    /* Handles rotation by, for now, the cursor position. */
+    void Rotation(){
+        Vector3 target = Input.mousePosition;
+        float hypotenuse = Mathf.Pow(Mathf.Pow(target.x - screenPos.x, 2) + Mathf.Pow(target.y - screenPos.y, 2), 0.5f);
+
+        //JUST REALIZED AFTER MAKING THE BELOW CODE: On a unit circle in Unity, 0degrees would be at point (0, 1), and 270degrees at (1, 0);
+        if (target.y < screenPos.y && target.x > screenPos.x)
+            transform.rotation = Quaternion.Euler(0, 0, 270 - Mathf.Rad2Deg * Mathf.Acos((target.x - screenPos.x) / hypotenuse));
+        else if (target.y < screenPos.y && target.x < screenPos.x)
+            transform.rotation = Quaternion.Euler(0, 0, 90 + Mathf.Rad2Deg * Mathf.Atan((target.y - screenPos.y)/(target.x - screenPos.x)));
+        else if (target.y > screenPos.y && target.x > screenPos.x)
+            transform.rotation = Quaternion.Euler(0, 0, 270 + Mathf.Rad2Deg * Mathf.Asin((target.y - screenPos.y) / hypotenuse));
+        else
+            transform.rotation = Quaternion.Euler(0, 0, 90 - Mathf.Rad2Deg * Mathf.Asin((target.y - screenPos.y) / hypotenuse));
+    }
 	/* Handles directional user input by checking the keycodes. Also handles bounds. */
 	void Movement() {
-		if (Input.GetKey(up) && transform.position.y < 12) {
-			transform.Translate(Vector2.up * speed); }
-		else if (Input.GetKey(down) && transform.position.y > -12) {
-			transform.Translate(-Vector2.up * speed); }
-		if (Input.GetKey(left) && transform.position.x > -32) {
-			transform.Translate(-Vector2.right * speed); }
-		else if (Input.GetKey(right) && transform.position.x < 32) {
-			transform.Translate(Vector2.right * speed); }
+		if (Input.GetKey(up) && transform.position.y < 12)
+			transform.Translate(Vector2.up * speed, Space.World);
+		else if (Input.GetKey(down) && transform.position.y > -12)
+			transform.Translate(-Vector2.up * speed, Space.World);
+		if (Input.GetKey(left) && transform.position.x > -32)
+			transform.Translate(-Vector2.right * speed, Space.World);
+		else if (Input.GetKey(right) && transform.position.x < 32)
+			transform.Translate(Vector2.right * speed, Space.World);
+
+        screenPos = camera.WorldToScreenPoint(transform.position);
 	}
 
 	/* Destroys the player; is called after the sound plays. */
@@ -35,9 +56,7 @@ public class Player : MonoBehaviour {
 
 	/* Checks for collisions with bullets. */
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Bullet") {
+		if (coll.gameObject.tag == "Bullet")
 			audio.PlayOneShot(lose);
-			//Invoke("Destroy", lose.length);
-		}
 	}
 }
